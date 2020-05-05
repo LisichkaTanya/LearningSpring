31aae319caae0e76f0d5b832d1a38da4489e5519
LisichkaTanya <lisichka-tanya@mail.ru>
2020-05-05 09:42:47 +0300

    use properties in resours's file

--- src/main/java/com/lisichka/MusicPlayer.java
+++ src/main/java/com/lisichka/MusicPlayer.java
@@ -2,10 +2,36 @@ package com.lisichka;
 
 public class MusicPlayer {
     private Music music;
+    private String name;
+    private int volume;
+
+    public MusicPlayer(){}
+
     public MusicPlayer (Music music){
         this.music = music;
     }
+
+    public void setMusic(Music music) {
+        this.music = music;
+    }
+
     public void playMusic (){
         System.out.println(music.getSong());
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
     }
 }
---
+++ src/main/java/com/lisichka/jazzMusic.java
@@ -0,0 +1,9 @@
+package com.lisichka;
+
+public class jazzMusic implements Music {
+
+    @Override
+    public String getSong() {
+        return "Francis Albert Sinatra";
+    }
+}
